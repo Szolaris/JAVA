55d14d89b07f65ab4bbc147fa20c983d3fd7842e
Szolár Peti <szolarpeter@gmail.com>
2022-10-26 21:57:13 +0200

    userId es employeeId letrehozva

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -3,12 +3,35 @@ package employee;
 public class Employee {
     private int salary;
     private int workHoursPerDay;
-    public Employee(int salary, int workHoursPerDay) {
-        this.salary = salary;
+    private int employeeId;
+    private int userId = 33;
+
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+    public Employee(int salarrrrry, int workHoursPerDay, int userID ) {
+        this.userId = userID;
+        this.salary = salarrrrry;
         this.workHoursPerDay = workHoursPerDay;
     }
+
     public void getInfo() {
-        System.out.println("Fizetes: " + salary + "Ft, Napi munkaideje: " + workHoursPerDay);
+        System.out.println("userId =" + getUserId() + "   emloyeeId = " + this.employeeId + "    Fizetes: " + salary + "Ft, Napi munkaideje: " + workHoursPerDay);
     }
     public void addSalary(int addAmount, int limit) {
         if (salary < limit) {
@@ -18,7 +41,9 @@ public class Employee {
 
     public void addWork() {
         if (workHoursPerDay >= 6) {
-            salary += 5;
+            for (int i = 0; i < workHoursPerDay-5; i++) {
+                salary += 5;
+            }
         }
     }
 }
--- src/employee/EmployeeTest.java
+++ src/employee/EmployeeTest.java
@@ -2,10 +2,27 @@ package employee;
 
 public class EmployeeTest {
     public static void main(String[] args) {
-        Employee dolgozo = new Employee(490, 6);
-        dolgozo.addSalary(10, 500);
-        dolgozo.addSalary(200, 600);
-        dolgozo.addWork();
-        dolgozo.getInfo();
+        Employee dolgozo1 = new Employee(490, 20, 31);
+        Employee dolgozo2 = new Employee(900, 4,50);
+        dolgozo1.setEmployeeId(568854);
+        dolgozo1.addSalary(10, 500);
+        dolgozo1.addSalary(200, 600);
+        dolgozo1.addWork();
+        dolgozo1.getInfo();
+        dolgozo2.setEmployeeId(34526);
+        dolgozo2.setUserId(44);
+        dolgozo2.addSalary(10, 500);
+        dolgozo2.addSalary(200, 600);
+        dolgozo2.addWork();
+        dolgozo2.getInfo();
+
+
+
+
+
+
     }
+
+
+
 }
